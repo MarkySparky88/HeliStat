36c9984a1bac25922baa5618d97e66974a2232d1
marcpreisig <marcpreisig@hotmail.com>
2020-01-08 21:51:46 +0100

    Begin of switching to access db

--- HeliStat/Properties/Settings.Designer.cs
+++ HeliStat/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace HeliStat.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -43,6 +43,17 @@ namespace HeliStat.Properties {
             }
             set {
                 this["ActualYear"] = value;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\db\\HeliStat_db1.acc" +
+            "db")]
+        public string HeliStat_db1_ConnectionString {
+            get {
+                return ((string)(this["HeliStat_db1_ConnectionString"]));
             }
         }
     }
